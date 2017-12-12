79d06a9795122c38526f01bf6561da27c0ed420f
Diana Flores García <diana_1310fg@hotmail.com>
2017-12-12 07:51:33 -0600

    Ya ahora si están todas las clases, deberás, deberitas. Y está vez si es en serio.

--- src/main/java/unitec/org/sistemaescolar/SistemaescolarApplication.java
+++ src/main/java/unitec/org/sistemaescolar/SistemaescolarApplication.java
@@ -10,13 +10,16 @@ public class SistemaescolarApplication implements CommandLineRunner{
 @Autowired RepositorioAlumno repoAlumno;
 @Autowired RepositorioProfesor repoProfesor;
 @Autowired RepositorioCurso repoCurso;
+@Autowired RepositorioCursoAlumno repoCursoAlumno;
     public static void main(String[] args) {
         SpringApplication.run(SistemaescolarApplication.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        Curso curso = new Curso("2564", "256", "JAVA", "PROGRAMACION", "20 Junio de 2017", "20 de Agosto de 2017", "1:00 pm", "Lunes y Jueves", 1500, "5", "10");
-        repoCurso.save(curso);
+        /*Curso curso = new Curso("2564", "256", "JAVA", "PROGRAMACION", "20 Junio de 2017", "20 de Agosto de 2017", "1:00 pm", "Lunes y Jueves", 1500, "5", "10");
+        repoCurso.save(curso);*/
+        /*CursoAlumno cursoAlumno = new CursoAlumno(65164, 4564, "rv46v", 5.8f);
+        repoCursoAlumno.save(cursoAlumno);*/
     }   
 }
